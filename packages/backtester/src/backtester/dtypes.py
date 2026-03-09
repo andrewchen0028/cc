04fd023e7d2ec7ddcfcfe2b937c311541c31264e
Andrew Chen <andrewchen0028@gmail.com>
2026-03-09 16:38:52 -0400

    add Order and Fill dtypes, and UUIDs

--- packages/backtester/src/backtester/dtypes.py
+++ packages/backtester/src/backtester/dtypes.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from datetime import datetime, time
+from enum import StrEnum
 from typing import Literal
+
 
 from utils import checks
 
@@ -37,3 +39,58 @@ class SpotInstrument:
 
 
 Instrument = OptionInstrument | SpotInstrument
+
+
+class Side(StrEnum):
+    BUY = "buy"
+    SELL = "sell"
+
+
+@dataclass(frozen=True, slots=True)
+class MarketOrder:
+    t: datetime
+    i: Instrument
+    q: float
+    side: Side
+
+    def __post_init__(self) -> None:
+        checks.require(
+            checks.is_utc("t", self.t),
+            checks.is_gt("q", self.q, "0", 0),
+        )
+
+
+@dataclass(frozen=True, slots=True)
+class LimitOrder:
+    t: datetime
+    i: Instrument
+    q: float
+    side: Side
+    px_limit: float
+
+    def __post_init__(self) -> None:
+        checks.require(
+            checks.is_utc("t", self.t),
+            checks.is_gt("q", self.q, "0", 0),
+            checks.is_gt("px_limit", self.px_limit, "0", 0),
+        )
+
+
+Order = MarketOrder | LimitOrder
+
+
+@dataclass(frozen=True, slots=True)
+class Fill:
+    t: datetime
+    i: Instrument
+    o: Order
+    q: float
+    side: Side
+    px: float
+
+    def __post_init__(self) -> None:
+        checks.require(
+            checks.is_utc("t", self.t),
+            checks.is_gt("q", self.q, "0", 0),
+            checks.is_gt("px", self.px, "0", 0),
+        )
